df3bf0055bf0535ca3cb831b55f914e0f8bc425b
Sumer Ivan <sumer.ivan@gmail.com>
2021-10-09 20:40:46 +0300

    Add Core and Infrastructure projects

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Entities;
+using Infrastructure.Data;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            return _context.Products.ToList();
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            return _context.Products.Find(id);
+        }
+    }
+}
